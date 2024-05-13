7a1d642eba498cc8b0919167a0177170098ba527
Sumit Kumar <bingostudypoint@gmail.com>
2024-05-13 14:32:43 +0530

    learn arrow function and add HOF

--- newasign.js
+++ newasign.js
@@ -43,3 +43,14 @@ function higherOrder2() {
 }      
 var x = higherOrder2();
 x()  
+
+var Person = function(pName){
+  var name = pName;
+
+  this.getName = function(){
+    return name;
+  }
+}
+
+var person = new Person("Neelesh");
+console.log(person.getName());
